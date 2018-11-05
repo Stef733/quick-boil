7bde951407b65f5bfb5e2301ba17037652ca4213
SG <you@example.com>
2018-11-05 18:20:08 +0000

    feat: redux thunks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import appReducers from './reducers';
 
 // @ts-ignore - the following window property exists only when Redux dev tools extension is installed
@@ -7,4 +8,5 @@ const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_E
 export default createStore(
 	appReducers,
 	devTool,
+	applyMiddleware(thunkMiddleware),
 );
